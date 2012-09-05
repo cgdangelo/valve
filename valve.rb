6f06b7eb28bddc16a20b0a9983938da5a6070c6f
Charles D'Angelo <c.g.dangelo@gmail.com>
2012-09-04 22:27:12 -0400

    Add broken styles for popout menus, coffee-script gem

--- valve.rb
+++ valve.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'sinatra'
+require 'coffee-script'
 
 set :haml, :format => :html5
 
@@ -9,4 +10,9 @@ end
 
 get '/styles.css' do
   sass :styles
+end
+
+get '/js/nav.js' do
+  content_type 'text/javascript'
+  coffee :nav
 end
